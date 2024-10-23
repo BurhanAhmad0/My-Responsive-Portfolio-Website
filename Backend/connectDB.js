ed5d79e70829f7cdae63499ac6155fdc72c3e1b6
Burhan Ahmad <burhan7554@gmail.com>
2024-10-23 08:41:33 -0700

    Backend fix

--- Backend/connectDB.js
+++ Backend/connectDB.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     try {
 
-        let conn = await mongoose.connect('mongodb+srv://burhanvu57:wKAQvzNQNhDJn4dh@portfolio.synp2.mongodb.net/?retryWrites=true&w=majority&appName=Portfolio')
+        let conn = await mongoose.connect('mongodb+srv://burhanvu57:wKAQvzNQNhDJn4dh@portfolio.synp2.mongodb.net/ContactForm?retryWrites=true&w=majority&appName=Portfolio')
         console.log('Succesfull Connection!')
         
     } catch (error) {
